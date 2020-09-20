19507b8cbd4e5a811bc71e3a48c272806749cbd2
Chris <christobal_c@hotmail.com>
2020-09-20 21:10:46 +0200

    material ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,55 @@
+import React from "react";
 import Head from "next/head";
 import connectToDatabase from "../util/mongodb";
 import Nav from "../components/nav";
-import styles from "../styles/Home.module.css";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import CommentIcon from "@material-ui/icons/Comment";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 export default function Home({ isConnected }) {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState([0]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
   return (
     <div className={"container"}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </Head>
 
       <main className={"main"}>
@@ -20,6 +61,38 @@ export default function Home({ isConnected }) {
             </h1>
           </div>
         </div>
+
+        <List className={classes.root}>
+          {[0, 1, 2, 3].map((value) => {
+            const labelId = `checkbox-list-label-${value}`;
+
+            return (
+              <ListItem
+                key={value}
+                role={undefined}
+                dense
+                button
+                onClick={handleToggle(value)}
+              >
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(value) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ "aria-labelledby": labelId }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="comments">
+                    <CommentIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            );
+          })}
+        </List>
 
         {isConnected ? (
           <h2 className="subtitle">You are connected to MongoDB</h2>
