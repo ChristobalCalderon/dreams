51e2d8356fa8dd3463768c0bf4f208e10effbcb6
Chris <christobal_c@hotmail.com>
2020-10-06 22:25:01 +0200

    add everything

---
+++ pages/statistics.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Statistics = () => {
+  return (
+    <div>
+      <Layout></Layout>;
+    </div>
+  );
+};
+
+export default Statistics;
